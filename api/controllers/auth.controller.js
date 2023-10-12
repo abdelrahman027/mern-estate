2fc1efd903bc3b8ad0664eb8bf3aab54d2398132
abdelrahman027 <abdelrahmansaad027@gmail.com>
2023-10-13 00:42:58 +0300

    adding auth route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
+
+export const signup =async (req,res)=>{
+   const {username,email,password} = req.body;
+   const hashedPassword = bcrypt.hashSync(password,10)
+   try{
+       const newUser =  new User({username,email,password:hashedPassword})
+       await newUser.save();
+       res.status(201).json("user created successfully")
+   } catch (err) {
+    console.log(err)
+   }
+
+}
--- api/index.js
+++ api/index.js
@@ -2,14 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
 dotenv.config();
 
 const app = express();
+app.use(express.json())
 
-app.use(userRoutes);
+app.use('/api/user',userRoutes);
 
 
-
+app.use('/api/auth',authRoutes)
 
 
 
