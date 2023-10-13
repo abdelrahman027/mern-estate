a3eee4faff6a01643fbd598464608466d1aef31e
abdelrahman027 <abdelrahmansaad027@gmail.com>
2023-10-13 14:17:29 +0300

    error handling handle

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
 
-export const signup =async (req,res)=>{
+export const signup =async (req,res,next)=>{
    const {username,email,password} = req.body;
    const hashedPassword = bcrypt.hashSync(password,10)
    try{
@@ -9,7 +9,7 @@ export const signup =async (req,res)=>{
        await newUser.save();
        res.status(201).json("user created successfully")
    } catch (err) {
-    console.log(err)
+    next(error)
    }
 
 }
