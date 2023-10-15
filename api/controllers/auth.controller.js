16d35649ea1fa67054b6d222bec98e12a8f9dea7
abdelrahman027 <abdelrahmansaad027@gmail.com>
2023-10-15 15:52:43 +0300

    sign up page func

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,16 @@
+/** @format */
+
 import User from "../models/user.model.js";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
-export const signup =async (req,res,next)=>{
-   const {username,email,password} = req.body;
-   const hashedPassword = bcrypt.hashSync(password,10)
-   try{
-       const newUser =  new User({username,email,password:hashedPassword})
-       await newUser.save();
-       res.status(201).json("user created successfully")
-   } catch (err) {
-    next(error)
-   }
-
-}
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  try {
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json("user created successfully");
+  } catch (err) {
+    next(err);
+  }
+};
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,29 +1,88 @@
-import { Link } from "react-router-dom";
+/** @format */
+
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [signupData, setSignupData] = useState({});
+  const [fetchError, setFetchError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSignupData({
+      ...signupData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signupData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setFetchError(data.message);
+        return;
+      }
+      setLoading(false);
+      setFetchError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setFetchError(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl font-semibold my-7 text-center">
-        Sign Up
-      </h1>
-      <form className="flex flex-col gap-4">
-        <input type="text" id="username" placeholder="Username" className="p-3 border rounded-lg" />
-        <input type="email" id="email" placeholder="Email" className="p-3 border rounded-lg" />
-        <input type="password" id="password" placeholder="Password" className="p-3 border rounded-lg" />
+      <h1 className="text-3xl font-semibold my-7 text-center">Sign Up</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="p-3 border rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="p-3 border rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="p-3 border rounded-lg"
+          onChange={handleChange}
+        />
         <button
+          disabled={loading}
           className="bg-slate-700 text-white p-3 rounded lg hover:bg-slate-600 uppercase disabled:opacity-80"
-        >Sign up</button>
+        >
+          {loading ? "loading" : "Sign up"}
+        </button>
+        {fetchError && <p className="text-red-500">{fetchError}</p>}
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an acount?</p>
-        <Link to={'/sign-in'}>
-          <span className="text-blue-700 hover:underline">
-            Sign in
-          </span>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700 hover:underline">Sign in</span>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
