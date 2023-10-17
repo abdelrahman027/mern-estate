95b08db07faa3075d96055a3604af160bc79fdbd
Abdelrahman <abdelrahmansaad027@gmail.com>
2023-10-17 17:34:46 +0300

    add persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-unused-vars */
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import userReducer from './user/userSlice.js'
+import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import userReducer from './user/userSlice.js';
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const persistedUser = persistReducer(persistConfig, rootReducer)
+
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer
-    },
+    reducer: persistedUser,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     }),
-})
+});
+
+export const persistor = persistStore(store)
