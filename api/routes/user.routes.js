8fc923209741e34f6d7c05c97d5a64db9c16ce81
abdelrahman027 <abdelrahmansaad027@gmail.com>
2023-11-09 10:31:10 +0200

    add contact functionality

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,6 +3,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getUserListings,
   test,
   updateUser,
@@ -15,4 +16,5 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 export default router;
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,9 +1,55 @@
+/** @format */
 
+import { Fragment, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-const Contact = () => {
-    return (
-        <div>Contact</div>
-    )
-}
+const Contact = ({ listing }) => {
+  const [owner, setOwner] = useState(null);
+  const [message, setMessage] = useState("");
 
-export default Contact
+  const fetchUser = async () => {
+    try {
+      const { userRef } = listing;
+      const res = await fetch(`/api/user/${userRef}`);
+      const userData = await res.json();
+      setOwner(userData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <Fragment>
+      {owner && (
+        <div>
+          <p>
+            Contact{" "}
+            <span className="text-lg font-semibold">{owner.username}</span>{" "}
+            Asking about{" "}
+            <span className="text-xl font-semibold">{listing.name}</span>
+          </p>
+          <textarea
+            name=""
+            id=""
+            rows={2}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="w-full border p-3 rounded-lg my-4"
+            placeholder="Enter message here"
+          ></textarea>
+          <Link
+            to={`mailto:${owner.email}?subject=Regarding ${listing.name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+          >
+            Send Message
+          </Link>
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+export default Contact;
