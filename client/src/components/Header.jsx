f1a0c2084cf57887034ed86062e2b1b45b05138a
abdelrahman027 <abdelrahmansaad027@gmail.com>
2023-11-10 14:42:03 +0200

    bind search form with url

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,34 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Header = () => {
   const [toggleDrop, setToggleDrop] = useState(false);
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
   const toggleDropHandler = () => {
     setToggleDrop(!toggleDrop);
   };
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -23,13 +40,20 @@ const Header = () => {
           </h1>
         </Link>
         {/* search */}
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSearch}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <AiOutlineSearch className="text-slate-600" />
+          <button onClick={handleSearch}>
+            <AiOutlineSearch className="text-slate-600" />
+          </button>
         </form>
         {/* list */}
         <ul className="md:flex gap-4 hidden">
